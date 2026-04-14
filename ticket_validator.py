580471044b2bc79ee19093e1f2a0f98966288f8a
Marcus Prado <mgprado1@my.waketech.edu>
2026-04-14 12:25:18 +0000

    worked on validate_ticket

--- ticket_validator.py
+++ ticket_validator.py
@@ -1,6 +1,20 @@
 def validate_ticket(code):
-    pass
+    if isinstance(code, str):
+        pass
+    else:
+        raise TypeError("Invalid ticket")
+    
+    if code[0] != "T" or code[1] != "K":
+        return False
+    elif (len(code) > 8 or len(code) < 8):
+        return False
 
+    for value in code[2:]:
+        if value.isdigit():
+            continue
+        else:
+            return False
+        
 def get_ticket_tier(code):
     pass
 
